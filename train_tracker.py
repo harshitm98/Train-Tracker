b3691bc4db083ba371153ffd3750f1ebcce5a2d7
harshitm98 <harshitmah98@gmail.com>
2017-12-25 01:02:49 +0530

    Succesfully stores the data in list

--- train_tracker.py
+++ train_tracker.py
@@ -6,14 +6,37 @@ Created on Sun Dec 24 20:57:24 2017
 """
 
 from urllib.request import Request, urlopen, URLError
+import json
 
 train_list=[16382]
+
+
 
 request = Request('https://api.railwayapi.com/v2/live/train/16382/date/24-12-2017/apikey/nxhcd5deez/')
 
 try:
-	response = urlopen(request)
-	jsonResponse = response.read()
-	print (jsonResponse)
+    response = urlopen(request)
+    print("Opening the requested url...")
+    jsonResponse = response.read()
+    print("Reading the response...")
+    loadedJson = json.loads(jsonResponse)
+    print("Loading the JSON response...")
+
+    response_code = loadedJson['response_code']
+    route = loadedJson['route']
+    
+    nameList = []
+    expectedArrival = []
+    actualArrival=[]
+    finalDelay=[]
+    for i in route:
+        if(i['has_arrived']):
+            nameList.append(i['station']['name'])
+            expectedArrival.append(i['scharr'])
+            actualArrival.append(i['actarr'])
+            finalDelay.append(int(i['status'].split(' ')[0]))
+    
+        
+    
 except (URLError, e):
     print ('Some error!', e)
