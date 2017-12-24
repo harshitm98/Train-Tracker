b105e37ecf89edf81d8dfdf5d31bcdf9066237f7
harshitm98 <harshitmah98@gmail.com>
2017-12-24 21:40:23 +0530

    Receives the json response

--- train_tracker.py
+++ train_tracker.py
@@ -5,3 +5,15 @@ Created on Sun Dec 24 20:57:24 2017
 @author: Harshit Maheshwari
 """
 
+from urllib.request import Request, urlopen, URLError
+
+train_list=[16382]
+
+request = Request('https://api.railwayapi.com/v2/live/train/16382/date/24-12-2017/apikey/nxhcd5deez/')
+
+try:
+	response = urlopen(request)
+	jsonResponse = response.read()
+	print (jsonResponse)
+except (URLError, e):
+    print ('Some error!', e)
